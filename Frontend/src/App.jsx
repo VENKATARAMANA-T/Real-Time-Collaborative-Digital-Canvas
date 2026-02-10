d0ad31778e3e18ebdbd19f1310dd036c9dc9d85d
T Venkataramana <tvenkataramana805@gmail.com>
2026-02-10 22:13:13 +0530

    Updated

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,15 @@
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import PaintApp from './pages/PaintApp.jsx';
-import HelpSystemUI from './pages/HelpSystemUI.jsx';
+import HelpSystemUI from './pages/HelpSystemUI.jsx'; 
+
 function App() {
   
 
  return (
     <Router>
       <Routes>
-
+        <Route path="/" element={<HelpSystemUI />} />
         <Route path="/paint" element={<PaintApp />} />
        
       </Routes>
