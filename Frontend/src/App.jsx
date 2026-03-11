d490debf2a77c0eae5a9d0d2ed367de52bcc02cc
T Venkataramana <tvenkataramana805@gmail.com>
2026-03-11 10:14:59 +0530

    Updated Frontend

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import PaintApp from './pages/PaintApp.jsx';
 import SharedCanvas from './pages/SharedCanvas.jsx';
 import Meeting from './pages/Meeting.jsx';
